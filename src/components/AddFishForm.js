2dc8a26d2f1494f640e48e437ba58f7bc3bb15c1
Jay Koontz <jkoontz2010@gmail.com>
2016-11-03 17:23:33 -0700

    added form components and integrated router to StorePicker

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+
+	createFish(event) {
+		event.preventDefault();
+		console.log("gonna make some fiyush!");
+
+		const fish = {
+			name: this.name.value,
+			pric: this.price.value,
+			status: this.status.value,
+			desc: this.desc.value,
+			image: this.image.value
+		}
+
+	}
+
+	render() {
+		return (
+			
+			<form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+				<input type="text" ref={(input) => this.name = input } placeholder="Fish Name" />
+				<input type="text" ref={(input) => this.price = input }placeholder="Fish Price" />
+				<select ref={(input) => this.status = input }>
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea ref={(input) => this.desc = input } placeholder="Fish Desc" />
+				<input ref={(input) => this.image = input } type="text" placeholder="Fish Image" />
+				<button type="submit">+ Add Item</button>
+			</form>
+			
+		)
+	}
+}
+
+export default AddFishForm;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+
+	goToStore(event) {
+		event.preventDefault();
+		const storeId = this.storeInput.value;
+
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" />
+				<input type="text" ref={(input) => { this.storeInput = input }} required placeholder="Store Name" />
 				<button type="submit">Visit Store -></button>
 			</form>
-			)
+		)
 	}
+}
+
+StorePicker.contextTypes ={
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
