6fb6fe554531b034e74976899575bf641916eb75
Jay Koontz <jkoontz2010@gmail.com>
2016-11-03 20:51:07 -0700

    introduced state

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -14,12 +14,14 @@ class AddFishForm extends React.Component {
 			image: this.image.value
 		}
 
+		this.props.addFish(fish);
+		this.fishForm.reset();
 	}
 
 	render() {
 		return (
 			
-			<form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+			<form ref={(input) => this.fishForm = input } className="fish-edit" onSubmit={(e) => this.createFish(e)}>
 				<input type="text" ref={(input) => this.name = input } placeholder="Fish Name" />
 				<input type="text" ref={(input) => this.price = input }placeholder="Fish Price" />
 				<select ref={(input) => this.status = input }>
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,37 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
+
+	constructor() {
+		super();
+
+		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+		// getinitialstate
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish) {
+		const fishes = {...this.state.fishes};
+
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+
+		this.setState({ fishes });
+	}
+
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -11,7 +41,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 			)
 	}
